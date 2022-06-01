736bfc1228ee78b83369f90dcf1ab37af9714492
Harald Reingruber <harald.reingruber@dedalus.com>
2022-06-01 13:30:14 +0200

    First pixel operation

--- src/main/java/org/example/ImageBlur.java
+++ src/main/java/org/example/ImageBlur.java
@@ -1,11 +1,15 @@
 package org.example;
 
-import org.apache.commons.imaging.*;
+import org.apache.commons.imaging.ImageFormats;
+import org.apache.commons.imaging.ImageReadException;
+import org.apache.commons.imaging.ImageWriteException;
+import org.apache.commons.imaging.Imaging;
 import org.apache.commons.imaging.formats.png.PngImageParser;
 import org.apache.commons.imaging.formats.png.PngImagingParameters;
 
 import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
+import java.awt.image.DataBuffer;
+import java.awt.image.DataBufferInt;
 import java.io.File;
 import java.io.IOException;
 
@@ -14,8 +18,33 @@ public class ImageBlur {
     public static void blurImage() {
         var filenameWithoutExtension = "./baboon";
         var image = readImage(filenameWithoutExtension+".png");
-        // var pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+        DataBuffer dataBuffer = image.getRaster().getDataBuffer();
+        int[] pixels = ((DataBufferInt) dataBuffer).getData();
+        processBlurFilter(pixels);
         writeImage(filenameWithoutExtension+"-blurred.png", image);
+    }
+
+    private static void processBlurFilter(int[] pixels) {
+        for (int index = 0; index < pixels.length; index++) {
+            var pixel = new byte[] {Byte.MAX_VALUE, Byte.MIN_VALUE, Byte.MIN_VALUE, Byte.MIN_VALUE};
+            pixels[index] = byteArray2Int(pixel);
+        }
+    }
+
+    // from https://stackoverflow.com/questions/2183240/java-integer-to-byte-array
+    public static byte[] intT2ByteArray(int value) {
+        return new byte[] {
+                (byte)(value >> 24),
+                (byte)(value >> 16),
+                (byte)(value >> 8),
+                (byte)value};
+    }
+
+    public static int byteArray2Int(byte[] array) {
+        return array[0] << 24
+                | array[1] << 16
+                | array[2] << 8
+                | array[3];
     }
 
     private static void writeImage(String path, BufferedImage image) {
