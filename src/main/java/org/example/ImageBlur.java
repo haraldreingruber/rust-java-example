7eecd979f2550f7216d69fc7d3de6337ecadc801
Harald Reingruber <harald.reingruber@dedalus.com>
2022-06-01 14:05:23 +0200

    Implement grayscale filter

--- src/main/java/org/example/ImageBlur.java
+++ src/main/java/org/example/ImageBlur.java
@@ -12,35 +12,47 @@ import java.awt.image.DataBuffer;
 import java.awt.image.DataBufferInt;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class ImageBlur {
 
     public static void blurImage() {
         var filenameWithoutExtension = "./baboon";
-        var image = readImage(filenameWithoutExtension+".png");
+        var image = readImage(filenameWithoutExtension + ".png");
         DataBuffer dataBuffer = image.getRaster().getDataBuffer();
         int[] pixels = ((DataBufferInt) dataBuffer).getData();
-        processBlurFilter(pixels);
-        writeImage(filenameWithoutExtension+"-blurred.png", image);
+        processGrayscaleFilter(pixels);
+        writeImage(filenameWithoutExtension + "-blurred.png", image);
     }
 
     private static void processBlurFilter(int[] pixels) {
         for (int index = 0; index < pixels.length; index++) {
-            var pixel = new byte[] {Byte.MAX_VALUE, Byte.MIN_VALUE, Byte.MIN_VALUE, Byte.MIN_VALUE};
-            pixels[index] = byteArray2Int(pixel);
+            var pixel = new short[]{0, 0, 0, 255};
+            pixels[index] = ARGB2Int(pixel);
+        }
+    }
+
+    private static void processGrayscaleFilter(int[] pixels) {
+        for (int index = 0; index < pixels.length; index++) {
+            var pixel = int2ARGB(pixels[index]);
+            var grayColor = 0.299 * pixel[1]
+                    + 0.587 * pixel[2]
+                    + 0.114 * pixel[3];
+            Arrays.fill(pixel, (short) grayColor);
+            pixels[index] = ARGB2Int(pixel);
         }
     }
 
     // from https://stackoverflow.com/questions/2183240/java-integer-to-byte-array
-    public static byte[] intT2ByteArray(int value) {
-        return new byte[] {
-                (byte)(value >> 24),
-                (byte)(value >> 16),
-                (byte)(value >> 8),
-                (byte)value};
+    public static short[] int2ARGB(int value) {
+        return new short[]{
+                (short) Byte.toUnsignedInt((byte) (value >>> 24)),
+                (short) Byte.toUnsignedInt((byte) (value >>> 16)),
+                (short) Byte.toUnsignedInt((byte) (value >>> 8)),
+                (short) Byte.toUnsignedInt((byte) value)};
     }
 
-    public static int byteArray2Int(byte[] array) {
+    public static int ARGB2Int(short[] array) {
         return array[0] << 24
                 | array[1] << 16
                 | array[2] << 8
