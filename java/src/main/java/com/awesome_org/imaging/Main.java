e5ddef8d13764521bfc7517310ad52160907a4e9
Harald Reingruber <harald.reingruber@dedalus.com>
2022-06-10 18:02:30 +0200

    Annotate the main function a bit better

--- java/src/main/java/com/awesome_org/imaging/Main.java
+++ java/src/main/java/com/awesome_org/imaging/Main.java
@@ -21,6 +21,7 @@ public class Main {
     }
 
     public static void main(String... args) {
+        // loading image
         var image = ImageIO.readImage(FILENAME_WITHOUT_EXTENSION + ".png");
         DataBuffer dataBuffer = image.getRaster().getDataBuffer();
         int[] pixelsInt = ((DataBufferInt) dataBuffer).getData();
@@ -28,6 +29,7 @@ public class Main {
         byteBuffer.asIntBuffer().put(pixelsInt);
         byte[] pixels = byteBuffer.array();
 
+        // blur filter
         var result = new byte[pixels.length];
         var nanoTimeBefore = System.nanoTime();
         blurFilter.processBlurFilter(pixels, result, image.getWidth(), image.getHeight());
@@ -35,11 +37,13 @@ public class Main {
         System.arraycopy(result, 0, pixels, 0, pixels.length);
         System.out.println("Blur filter took " + processingTime / 1000000.0 + "ms");
 
+        // grayscale filter
         nanoTimeBefore = System.nanoTime();
         grayscaleFilter.processGrayscaleFilter(pixels);
         processingTime = System.nanoTime() - nanoTimeBefore;
         System.out.println("Grayscale filter took " + processingTime / 1000000.0 + "ms");
 
+        // saving image
         byteBuffer.asIntBuffer().get(pixelsInt);
         ImageIO.writeImage(FILENAME_WITHOUT_EXTENSION + "-blurred.png", image);
     }
