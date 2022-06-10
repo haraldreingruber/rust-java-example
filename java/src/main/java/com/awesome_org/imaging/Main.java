c0187eccfaace44534387a75f7c4ef750d82d192
Harald Reingruber <harald.reingruber@dedalus.com>
2022-06-10 17:29:43 +0200

    Fix actual demo-start version

--- java/src/main/java/com/awesome_org/imaging/Main.java
+++ java/src/main/java/com/awesome_org/imaging/Main.java
@@ -1,42 +1,34 @@
 package com.awesome_org.imaging;
 
-import com.awesome_org.imaging.filters.BlurFilter;
 import com.awesome_org.imaging.filters.GrayscaleFilter;
-import com.awesome_org.imaging.filters.JavaAverageBlurFilter;
-import com.awesome_org.imaging.filters.RustGrayscaleFilter;
+import com.awesome_org.imaging.filters.JavaGrayscaleFilter;
 
 import java.awt.image.DataBuffer;
 import java.awt.image.DataBufferInt;
 import java.nio.ByteBuffer;
 
 public class Main {
-    public static final String FILENAME_WITHOUT_EXTENSION = "./baboon";
+    public static final String BABOON_FILE_NAME = "./baboon";
 
-    private static BlurFilter blurFilter = new JavaAverageBlurFilter();
-
-    private static GrayscaleFilter grayscaleFilter = new RustGrayscaleFilter();
+    private static final GrayscaleFilter grayscaleFilter = new JavaGrayscaleFilter();
 
     public static void main(String... args) {
-        var image = ImageIO.readImage(FILENAME_WITHOUT_EXTENSION + ".png");
+        var image = ImageIO.readImage(BABOON_FILE_NAME + ".png");
         DataBuffer dataBuffer = image.getRaster().getDataBuffer();
         int[] pixelsInt = ((DataBufferInt) dataBuffer).getData();
         var byteBuffer = ByteBuffer.allocate(pixelsInt.length * 4);
         byteBuffer.asIntBuffer().put(pixelsInt);
         byte[] pixels = byteBuffer.array();
 
-        var result = new byte[pixels.length];
+  
         var nanoTimeBefore = System.nanoTime();
-        blurFilter.processBlurFilter(pixels, result, image.getWidth(), image.getHeight());
-        var processingTime = System.nanoTime() - nanoTimeBefore;
-        System.arraycopy(result, 0, pixels, 0, pixels.length);
-        System.out.println("Blur filter took " + processingTime / 1000000.0 + "ms");
 
-        nanoTimeBefore = System.nanoTime();
         grayscaleFilter.processGrayscaleFilter(pixels);
-        processingTime = System.nanoTime() - nanoTimeBefore;
+
+        var processingTime = System.nanoTime() - nanoTimeBefore;
         System.out.println("Grayscale filter took " + processingTime / 1000000.0 + "ms");
 
         byteBuffer.asIntBuffer().get(pixelsInt);
-        ImageIO.writeImage(FILENAME_WITHOUT_EXTENSION + "-blurred.png", image);
+        ImageIO.writeImage(BABOON_FILE_NAME + "-blurred.png", image);
     }
 }
